6b0d8f217d564dd77216992f7afb35f1143feeb2
stephanbissoon <stephan.bissoon@ryerson.ca>
2024-01-25 20:58:04 -0500

    Initial backend setup

---
+++ backend/src/main/java/com/cps630/nexus/NexusApplication.java
@@ -0,0 +1,11 @@
+package com.cps630.nexus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(NexusApplication.class, args);
+	}
+}
