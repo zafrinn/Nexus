3df473565d4b8eed08e327af9d211c7ed0605759
stephanbissoon <stephan.bissoon@ryerson.ca>
2024-01-26 14:37:30 -0500

    Database initialization

---
+++ backend/nexus_db.sql
@@ -0,0 +1,80 @@
+CREATE DATABASE IF NOT EXISTS nexus;
+
+USE nexus;
+
+DROP TABLE IF EXISTS advertisement_image;
+DROP TABLE IF EXISTS advertisement;
+DROP TABLE IF EXISTS category;
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS role;
+
+CREATE TABLE role (
+    role_id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(10) NOT NULL,
+
+    PRIMARY KEY(role_id),
+    UNIQUE(name)
+);
+
+INSERT INTO role VALUES(NULL, "Admin");
+INSERT INTO role VALUES(NULL, "Basic");
+
+CREATE TABLE user (
+    user_id INT NOT NULL AUTO_INCREMENT,
+    display_name VARCHAR(25) NOT NULL,
+    role_id INT NOT NULL,
+    email_address VARCHAR(50) NOT NULL,
+    password CHAR(60) NULL,
+    created_timestamp DATETIME NOT NULL,
+
+    PRIMARY KEY (user_id),
+    UNIQUE(email_address),
+    FOREIGN KEY (role_id) REFERENCES role(role_id)
+);
+
+INSERT INTO user VALUES(NULL, "stephan", 2, "stephan.bissoon@torontomu.ca", NULL, NOW());
+INSERT INTO user VALUES(NULL, "stephan_admin", 1, "stephan.bissoon+admin@torontomu.ca", NULL, NOW());
+INSERT INTO user VALUES(NULL, "zafrin", 2, "zafrin.rahman@torontomu.ca", NULL, NOW());
+INSERT INTO user VALUES(NULL, "zafrin_admin", 1, "zafrin.rahman+admin@torontomu.ca", NULL, NOW());
+INSERT INTO user VALUES(NULL, "alishba", 2, "alishba.aamir@torontomu.ca", NULL, NOW());
+INSERT INTO user VALUES(NULL, "alishba_admin", 1, "alishba.aamir+admin@torontomu.ca", NULL, NOW());
+INSERT INTO user VALUES(NULL, "shruti", 2, "s12sharma@torontomu.ca", NULL, NOW());
+INSERT INTO user VALUES(NULL, "shruti_admin", 1, "s12sharma+admin@torontomu.ca", NULL, NOW());
+INSERT INTO user VALUES(NULL, "karanvir", 2, "karanvir.heer@torontomu.ca", NULL, NOW());
+INSERT INTO user VALUES(NULL, "karanvir_admin", 1, "karanvir.heer+admin@torontomu.ca", NULL, NOW());
+
+CREATE TABLE category (
+    category_id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(20) NOT NULL,
+
+    PRIMARY KEY (category_id),
+    UNIQUE(name)
+);
+
+INSERT INTO category VALUES(NULL, "Items Wanted");
+INSERT INTO category VALUES(NULL, "Items for Sale");
+INSERT INTO category VALUES(NULL, "Academic Services");
+
+CREATE TABLE advertisement (
+    advertisement_id INT NOT NULL AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    title VARCHAR(50) NOT NULL,
+    description VARCHAR(1000) NOT NULL,
+    created_timestamp DATETIME NOT NULL,
+    category_id INT NOT NULL,
+    price DECIMAL(10, 2) NULL,
+    location VARCHAR(100) NULL,
+
+    PRIMARY KEY(advertisement_id),
+    FOREIGN KEY (user_id) REFERENCES user(user_id),
+    FOREIGN KEY (category_id) REFERENCES category(category_id)
+);
+
+CREATE TABLE advertisement_image (
+    advertisement_image_id INT NOT NULL AUTO_INCREMENT,
+    advertisement_id INT NOT NULL,
+    file_name VARCHAR(50), -- UUID name with .jpg/jpeg/png etc...
+
+    PRIMARY KEY (advertisement_image_id),
+    FOREIGN KEY (advertisement_id) REFERENCES advertisement(advertisement_id)
+);
