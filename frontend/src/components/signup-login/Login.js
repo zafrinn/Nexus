c754a192f8e5e6c87afed9ca32ed2582c1a39549
karanvirheer <karanvirheer77@gmail.com>
2024-03-28 14:00:01 -0400

    WIP Backend User Authentication

--- frontend/src/components/signup-login/Login.js
+++ frontend/src/components/signup-login/Login.js
@@ -1,99 +1,72 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./login-signup.css";
 import logo from "../../assets/logo.png";
-import slogan from "../../assets/slogan.png"
-import ReCAPTCHA from 'react-google-recaptcha';
+import slogan from "../../assets/slogan.png";
+import ReCAPTCHA from "react-google-recaptcha";
 
-const LoginPage = (userId, setUserId ) => {
-    
+const LoginPage = ({ setUserId }) => {
   const navigate = useNavigate();
   const [captchaValue, setCaptchaValue] = useState(null);
-    
+
   const handleCaptchaChange = (value) => {
-    console.log('Captcha value:', value);
+    console.log("Captcha value:", value);
     setCaptchaValue(value);
   };
 
-  const handleSubmit2 = (e) => {
-    e.preventDefault();
-    // Validate captchaValue and submit form if valid
-    if (captchaValue) {
-      console.log('Form submitted with captcha value:', captchaValue);
-      // Submit your signup form here
-    } else {
-      console.log('Please complete the CAPTCHA');
-    };
-
-  }
-
   const handleSubmit = async (e) => {
     e.preventDefault();
-    
+
     if (captchaValue) {
-      console.log('Form submitted with captcha value:', captchaValue);
-      
-      /*
+      console.log("Form submitted with captcha value:", captchaValue);
+
       const formData = {
         username: e.target.username.value,
         password: e.target.Password.value,
       };
-      console.log(formData);
-  
-      const response = await fetch("/api/login", {
-        method: "POST",
-        dataType: "json",
-        headers: {
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "OPTIONS, POST, GET",
-          "Access-Control-Allow-Headers": "Content-Type, Authorization",
-        },
-        body: JSON.stringify(formData),
-      });
-  
-      const responseData = await response.json();
-      */
 
+      try {
+        const response = await fetch(
+          "http://localhost:8080/api/v1/external/login",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(formData),
+          }
+        );
 
-      const responseData = {
-        "message": "User Logged In successfully",
-        "user_info": {
-          "userId": "123456",
-          "username": "example_user",
-          "email": "example@example.com"
+        // const responseData = await response.json();
+        // console.log(responseData);
+
+        if (response.ok) {
+          // console.log(responseData);
+          // setUserId(responseData.user_info.userId);
+          navigate("/dashboard");
+        } else {
+          // alert(responseData.error || "Login failed. Please try again.");
         }
+      } catch (error) {
+        console.error("Error:", error);
+        alert("An error occurred. Please try again later.");
       }
-      
-      if (responseData.message === "User Logged In successfully") {
-        console.log(responseData.user_info.userId);
-        //setUserId(responseData.user_info.userId);
-        //sessionStorage.setItem("userId", responseData.user_info.userId);
-        navigate("/dashboard");
-      } else if (
-        responseData.message === "User does not exist or Incorrect Password"
-      ) {
-        alert("User does not exist or Incorrect Password");
-      } else {
-        console.log("Email already in use");
-      }
-
-
     } else {
-      console.log('Please complete the CAPTCHA');
-      alert('Please complete the CAPTCHA');
-    };
-  }; 
+      console.log("Please complete the CAPTCHA");
+      alert("Please complete the CAPTCHA");
+    }
+  };
+
   return (
     <div className="container">
       <div className="row justify-content-center">
-        <div className="col-md-6" id="left-container"> 
+        <div className="col-md-6" id="left-container">
           <img id="logo" src={logo} alt="Logo" className="img-fluid" />
           <img id="slogan" src={slogan} alt="Slogan" className="img-fluid" />
         </div>
-        <div className ="col-md-6" id="right-container">
+        <div className="col-md-6" id="right-container">
           <div className="heading-container">Sign In</div>
           <div className="form-container">
             <form onSubmit={handleSubmit}>
@@ -117,9 +90,9 @@ const LoginPage = (userId, setUserId ) => {
                   placeholder="***********"
                   required
                 />
-                
+
                 <a href="/" id="forgotpass-instructions">
-                    <p id="forgotpass-instructions">Forgot your password</p>
+                  <p id="forgotpass-instructions">Forgot your password</p>
                 </a>
               </div>
               <ReCAPTCHA
@@ -135,12 +108,12 @@ const LoginPage = (userId, setUserId ) => {
             <div id="account">
               <p>Don't have an account yet?&nbsp;</p>
               <NavLink to="/signup">
-                <p style={{ marginTop: '3px', fontStyle: 'italic' }}>Sign Up</p>
+                <p style={{ marginTop: "3px", fontStyle: "italic" }}>Sign Up</p>
               </NavLink>
             </div>
           </div>
         </div>
-      </div>  
+      </div>
     </div>
   );
 };
