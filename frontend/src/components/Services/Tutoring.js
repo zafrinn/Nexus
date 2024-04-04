1308ed65f04c1519fb9c35ba326175e729482bd1
zafrinn <123769836+zafrinn@users.noreply.github.com>
2024-04-04 03:12:22 -0400

    Updated tutoring css

--- frontend/src/components/Services/Tutoring.js
+++ frontend/src/components/Services/Tutoring.js
@@ -62,14 +62,13 @@ function Tutoring() {
       <Grid item xs={12} sx={{ paddingRight: 2 }}>
         <Box maxWidth="100%" margin="auto" padding={2}>
           <Box sx={{ marginBottom: 4 }}>
-            <Box sx={{ display: 'flex', alignItems: 'flex-end', gap: 2, marginBottom: 2 }}>
+            <Box sx={{ display: 'flex', gap: 2, marginBottom: 2 }}>
               <TextField
                 label="Course Name"
                 variant="outlined"
                 value={courseName}
                 onChange={handleCourseNameChange}
                 fullWidth
-                sx={{ flex: '1 1 auto' }}
               />
               <TextField
                 select
@@ -78,7 +77,6 @@ function Tutoring() {
                 onChange={handleTutorLevelChange}
                 variant="outlined"
                 fullWidth
-                sx={{ flex: '1 1 auto' }}
               >
                 <MenuItem value="1">Beginner</MenuItem>
                 <MenuItem value="2">Intermediate</MenuItem>
@@ -90,7 +88,7 @@ function Tutoring() {
             </Box>
           </Box>
   
-          <Grid container spacing={6}>
+          <Grid container spacing={2} sx={{ marginTop: 2 }}>
             {tutorSessions.map((session) => (
               <Grid item xs={12} sm={6} md={3} key={session.tutorSessionId}>
                 <Card sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', height: '100%' }}>
@@ -104,7 +102,7 @@ function Tutoring() {
                       </Typography>
                     </CardContent>
                   </CardActionArea>
-                  <CardActions sx={{ justifyContent: 'flex-end', padding: '0 16px 8px' }}>
+                  <CardActions sx={{ justifyContent: 'center', padding: '0 14px 8px' }}>
                     <Button size="small" sx={{ color: '#003FA7', backgroundColor: 'white' }} onClick={() => handleContactButtonClick(session)}>
                       Contact
                     </Button>
